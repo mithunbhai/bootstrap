6fe0acbfa7f27c0a9a5ecdba5cc83f4539c091e8
md Shakif <bdshakif@gmail.com>
2017-01-05 10:29:16 +0600

    init

---
+++ address.php
@@ -0,0 +1,17 @@
+<?php 
+
+class myaddress{
+	
+	public function myName(){
+		echo 'My name is Sakif';
+	}
+
+	public function myAge(){
+		echo "my age is 21 years old";
+	}
+
+	public function address(){
+		echo "i am form Pabna";
+	}
+}
+
